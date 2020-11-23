5b36eb2158a3dff14ea88af904485c2e05bd3da4
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2020-11-23 16:01:59 -0500

    refactor: appease mypy

--- yams/parse.py
+++ yams/parse.py
@@ -1,7 +1,7 @@
 import oyaml as yaml  # type: ignore
 import os
 import collections
-from typing import Optional, Dict
+from typing import Optional, Dict, Any
 
 
 # ============== PARSING FUNCTIONS ======================
@@ -243,7 +243,7 @@ def create_params(
     *param_paths,
     error_on_unused: bool = False,
     out_path: Optional[str] = None,
-) -> Dict:
+) -> Dict[str,Any]:
     """
     Entry function - given the path to the parameter definitions and files, parse and create a params dictionary.
 
@@ -260,7 +260,7 @@ def create_params(
     """
     defs = build_yaml(defn_path)
 
-    params = {}
+    params: Dict[str,Any] = {}
     for param_path in param_paths:
         cur_params = build_yaml(param_path)
         params = merge(params, cur_params)
